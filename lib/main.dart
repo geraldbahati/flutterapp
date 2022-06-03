85366d5fbb3069ff5bb44dd575d6e09fc80eea81
Gerald Bahati <journeytoharvard@gmail.com>
2022-06-03 15:06:18 +0300

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firstapp/biews/login_view.dart';
-import 'package:firstapp/biews/register_view.dart';
 import 'package:firstapp/firebase_options.dart';
+import 'package:firstapp/views/login_view.dart';
+import 'package:firstapp/views/register_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +21,10 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     );
   }
 }
@@ -30,30 +34,28 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print('You are a verified email');
-              } else {
-                print('You need to verify your email first');
-              }
-              return const Text('Done');
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            // final user = FirebaseAuth.instance.currentUser;
+            // print(user);
+            // if (user?.emailVerified ?? false) {
+            //   print('You are a verified email');
+            //   return const Text('Done');
+            // } else {
+            //   print('You need to verify your email first');
+            //   return const VerifyEmailView();
+            // }
+            return const LoginView();
 
-            default:
-              return const Text('Loading...');
-          }
-        },
-      ),
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
---
+++ lib/views/register_view.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RegisterViewState createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            autocorrect: false,
+            enableSuggestions: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration:
+                const InputDecoration(hintText: 'Enter your email here'),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            autocorrect: false,
+            enableSuggestions: false,
+            decoration:
+                const InputDecoration(hintText: 'Enter your password here'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+              try {
+                final userCredential =
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                switch (e.code) {
+                  case 'weak-password':
+                    print('Weak password');
+                    break;
+
+                  case 'email-already-in-use':
+                    print('Email already being used');
+                    break;
+
+                  case 'invalid-email':
+                    print('invalid-email');
+                    break;
+                  default:
+                    print(e.code);
+                }
+              }
+            },
+            child: const Text('Register'),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/login/', (route) => false);
+              },
+              child: const Text('Already registered? Login here'))
+        ],
+      ),
+    );
+  }
+}
